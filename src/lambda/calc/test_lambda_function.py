56b2cb8575d64bc59d2cb25250e2b0c9f29d6cc1
jpatel <jignesh.p@dctinc.com>
2024-11-15 17:11:17 +0530

    Adding test_lambda_function.py

---
+++ src/lambda/calc/test_lambda_function.py
@@ -0,0 +1,33 @@
+from lambda_function import lambda_handler
+
+def test_lambda_handler1():
+    event = {
+        "first_name": "Krupal",
+        "last_name": "Patel",
+        "age": 21
+    }
+
+    output = {
+        "code": 200,
+        "message": "This is Krupal Patel, I am 21 years old."
+    }
+
+    response = lambda_handler(event=event, context=None)
+
+    assert response == output
+
+def test_lambda_handler2():
+    event = {
+        "first_name": "Kapish",
+        "last_name": "Patel",
+        "age": 25
+    }
+
+    output = {
+        "code": 200,
+        "message": "This is Kapish Patel, I am 25 years old."
+    }
+
+    response = lambda_handler(event=event, context=None)
+
+    assert response == output
